08ed272ad32594f8050b47408162e7364dc7062e
DoubleGit <vino_stargurl@yahoo.com>
2015-09-29 07:31:36 -1100

    Create inet.h

---
+++ inet.h
@@ -0,0 +1,28 @@
+/*	inet.h file
+	
+	TTP3121 TCP/IP Programming Project (Trimester 1, 2015/16) 
+	Members: 1) Venosha Arumugam	1112703549
+		 2) Aswini Suba Seelan	1121118896 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#define SERV_TCP_PORT 25000
+#define SERV_UDP_PORT 35001
+#define CLI_UDP_PORT 35002
+
+
+
+
+
+
+
+
